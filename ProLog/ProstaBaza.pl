bbd9ec55baa802bce26daca173d4d64fbb2c8fdb
Rafikus <rafikus95@gmail.com>
2018-06-05 11:46:05 +0200

    prolog

---
+++ ProLog/ProstaBaza.pl
@@ -0,0 +1,11 @@
+mezczyzna(marek).
+mezczyzna(antek).
+mezczyzna(franciszek).
+kobieta(ala).
+kobieta(anna).
+kobieta(maria).
+lubi(marek,psy).
+lubi(marek,cukierki).
+lubi(maria,ubrania).
+lubi(ala,cukierki).
+
---
+++ ProLog/zaleznosciRodzinne.pl
@@ -0,0 +1,30 @@
+mezczyzna(franciszek).
+kobieta(wanda).
+mezczyzna(jan).
+kobieta(maria).
+mezczyzna(bogdan).
+kobieta(anna).
+mezczyzna(krzysztof).
+mezczyzna(wojciech).
+kobieta(zofia).
+kobieta(ewa).
+kobieta(katarzyna).
+mezczyzna(robert).
+
+malzenstwo(jan,maria).
+rodzic(wojciech, jan).
+rodzic(wojciech, maria).
+rodzic(krzysztof, jan).
+rodzic(krzysztof, maria).
+
+malzenstwo(bogdan,anna).
+rodzic(zofia, bogdan).
+rodzic(ewa, bogdan).
+rodzic(zofia, anna).
+rodzic(ewa, anna).
+
+malzenstwo(wojciech,zofia).
+rodzic(katarzyna, wojciech).
+rodzic(robert, wojciech).
+rodzic(katarzyna, zofia).
+rodzic(robert, zofia).
